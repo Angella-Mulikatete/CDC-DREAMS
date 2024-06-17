f33317276313c19c848b13b90cd7215ea95274dc
Angella-Mulikatete <mulikateteangella@gmail.com>
2024-06-17 20:44:56 +0300

    added state context to manage statemanagement to determine which layout to display depending whether the user is authenticated or not

---
+++ cdc-dreams-frontend/src/context/contextProvider.tsx
@@ -0,0 +1,103 @@
+// since we have two layouts we need to differentiate which layout to display. It depends if the user is authenticared or not
+//React Context allows you to share and manage state across your components without passing down props.
+//Based on whether the  token is available or not, the default layout will be rendered or the guest layout will be rendered with corresponding pages
+
+
+import { createContext, useState, ReactNode, useContext } from "react";
+
+interface StateContextType {
+    user: object | null;
+    token: string | null;
+    setUser: React.Dispatch<React.SetStateAction<object | null>>;
+    setToken: (token: string | null) => void;
+}
+
+interface ContextProviderProps {
+    children: ReactNode;
+}
+
+
+const stateContext = createContext<StateContextType>({
+    user: null,
+    token: null,
+    setUser: () => {},
+    setToken: () => {},
+});
+
+export const ContextProvider: React.FC<ContextProviderProps> = ({ children }) => {
+    const [user, setUser] = useState<object | null>(null);
+    const [token, _setToken] = useState(localStorage.getItem("ACCESS_TOKEN"));
+
+    const setToken = (token: string | null) => {
+        _setToken(token);
+        if (token) {
+            localStorage.setItem("ACCESS_TOKEN", token); // whenever an authenticated user refreshes  he/she stays authenticated
+        } else {
+            localStorage.removeItem("ACCESS_TOKEN");
+        }
+    };
+
+    return (
+        <stateContext.Provider value={{ user, setUser, token, setToken }}>
+            {children}
+        </stateContext.Provider>
+    );
+};
+
+export const useStateContext = ()=> useContext(stateContext)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { createContext, useState } from "react";
+
+// const stateContext = createContext({
+//     user:null,
+//     token:null,
+// })
+
+// export const ContextProvider =({children})=>{
+//     const [user, setUser] = useState({});
+//     const [token, _setToken] = useState(localStorage.getItem('ACCESS_TOKEN'));
+
+//     const setToken = (token)=>{
+//         _setToken(token)
+//         if(token){
+//             localStorage.getItem('ACCESS_TOKEN', token);
+//         }else{
+//             localStorage.removeItem('ACCESS_TOKEN');
+//         }
+//     }
+//     return(
+//         <stateContext.Provider value={{
+//             user,
+//             setUser,
+//             token,
+//             setToken,
+//         }}>
+//             {children}
+//         </stateContext.Provider>
+//     )
+// }
--- cdc-dreams-frontend/src/router.tsx
+++ cdc-dreams-frontend/src/router.tsx
@@ -1,18 +1,43 @@
-import { createBrowserRouter } from "react-router-dom";
+import { Navigate, createBrowserRouter } from "react-router-dom";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Notfound from "./pages/Notfound";
+import Defaultlayout from "./components/Defaultlayout";
+import Guestlayout from "./components/Guestlayout";
+import Users from "./pages/Users";
 
 
 const routes = [
     {
-        path: "/login",
-        element: <Login />,
+        path: "/",
+        element: <Defaultlayout />,
+        children: [
+            {
+                path: "/",
+                element: <Navigate to="./users"/>,
+            },
+            {
+                path: "/users",
+                element: <Users />,
+            },
+        ],
     },
+
     {
-        path: "/signup",
-        element: <Signup />,
+        path: "/",
+        element: <Guestlayout />,
+        children: [
+            {
+                path: "/login",
+                element: <Login />,
+            },
+            {
+                path: "/signup",
+                element: <Signup />,
+            },
+        ],
     },
+
     {
         path: "*",
         element: <Notfound />,
