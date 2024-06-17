85590193e7be5861e1db3122cf139a50f7a720cf
Angella-Mulikatete <mulikateteangella@gmail.com>
2024-06-18 00:42:46 +0300

    Added a sign up form

--- cdc-dreams-frontend/src/pages/Login.tsx
+++ cdc-dreams-frontend/src/pages/Login.tsx
@@ -11,20 +11,20 @@ const Login = () => {
 
   return (
       <>
-          <div className="login-signup-form animated fadeInDown">
-              <div className="form">
-                  <h1 className="title">Login Form</h1>
-                  <form action="" onSubmit={onSubmit}>
-                      <input type="text" placeholder="Email" />
-                      <input type="password" placeholder="Password" />
-                      <button className="btn btn-block">Login</button>
-                      <p className="message">
-                          Not Registered?{" "}
-                          <Link to="/signup"> Create an account</Link>
-                      </p>
-                  </form>
-              </div>
+        <div className="login-signup-form animated fadeInDown">
+          <div className="form">
+            <h1 className="title">Login Form</h1>
+            <form action="" onSubmit={onSubmit}>
+              <input type="text" placeholder="Email" />
+              <input type="password" placeholder="Password" />
+              <button className="btn btn-block">Login</button>
+              <p className="message">
+                Not Registered?{" "}
+                <Link to="/signup"> Create an account</Link>
+              </p>
+            </form>
           </div>
+        </div>
       </>
   );
 }
--- cdc-dreams-frontend/src/pages/Signup.tsx
+++ cdc-dreams-frontend/src/pages/Signup.tsx
@@ -1,11 +1,33 @@
 //  
 
+import { Link } from "react-router-dom";
+
 const Signup = () => {
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+       event.preventDefault();
+  };
+
   return (
-    <>
-      <div>Sign up</div>
-    </>
-  )
+      <>
+          <div className="login-signup-form animated fadeInDown">
+              <div className="form">
+                  <h1 className="title">Sign Up Form</h1>
+                  <form action="" onSubmit={onSubmit}>
+                      <input type="text" placeholder="Full Name" />
+                      <input type="text" placeholder="Email" />
+                      <input type="password" placeholder="Password" />
+                      <input type="password" placeholder="Confirm Password" />
+                      <button className="btn btn-block">Register</button>
+                      <p className="message">
+                          Already Registered?
+                          <Link to="/login"> Login</Link>
+                      </p>
+                  </form>
+              </div>
+          </div>
+      </>
+  );
 }
 
 export default Signup
